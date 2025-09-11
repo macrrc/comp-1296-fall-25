e0c98a8b4ed69590da08b13def0af4c80834a7db
Chris Mac <cmac@rrc.ca>
2025-09-11 10:21:27 -0500

    add match/case example with condition

--- Week 3/match_case.py
+++ Week 3/match_case.py
@@ -14,7 +14,7 @@ match command:
 number = 10
 
 match number:
-    case number if number < 10:
+    case number if number < 10 and number < 0:
         print("number is less than 10")
     case number if number > 10:
         print("number is greater than 10")
